2b4e2d46ac8c1dbe315658a28ee41c8d4f21cfb6
moonfox1622 <moonfox1622@gmail.com>
2022-05-09 17:28:53 +0800

    加入專案檔案。

---
+++ CustomControls/Progress.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace CustomControls
+{
+    public enum TextPosition
+    {
+        right,
+        Right,
+        Center,
+        sliding,
+        None
+    }
+    class Progress : ProgressBar
+    {
+        //Fields
+        //Appearance
+        private Color channelColor = Color.LightSteelBlue;
+        private Color sliderColor = Color.RoyalBlue;
+        private Color foreBackColor = Color.RoyalBlue;
+        private int channelHeight = 6;
+        private int sliderHeight = 6;
+        private TextPosition showValue = TextPosition.Right;
+        private string symbolBefore = "";
+        private string symbolAfter = "";
+        private bool showMaximun = false;
+        //others
+        private bool paintedBack = false;
+        private bool stopPainting = false;
+
+    }
